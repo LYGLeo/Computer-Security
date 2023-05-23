bda8cd1812fb7c24c3df72986c9ee7f0d546b2a2
yglee <49851840+LYGLeo@users.noreply.github.com>
2023-05-24 00:32:00 +0900

    Add files via upload

--- alice.py
+++ alice.py
@@ -1,10 +1,10 @@
-import socket
+import socket, argparse, logging
 import os, sys
-import hmac
-import hashlib
+import hmac, hashlib
 from Crypto.Cipher import AES
 from time import sleep
 
+# initiate: python3 alice.py --addr 127.0.0.1  --port 6000 --enckey AAAAAAAAAAAAAAAA --mackey BBBBBBBBBBBBBBBB --iv CCCCCCCCCCCCCCCC
 #We use MAC then Encrypt
 
 ENCKEY_LENGTH = 16 #AES-128
@@ -26,28 +26,79 @@ for i in range(24):
   else:
     hours[i] = str(hours[i])
     
-
 label_pid = ["pid" for _ in range(24)]
 label_hour = ["hour" for _ in range(24)]
 label_lat = ["latitude" for _ in range(24)]
 label_lon = ["longitude" for _ in range(24)]
 
-def encrypt():
-  pass
+def encrypt(key, iv, msg):
+    pad = (BLOCK_LENGTH - len(msg)) % BLOCK_LENGTH # must be positive integer
+    msg = msg + pad * chr(pad).encode()
+    aes = AES.new(key.encode(), AES.MODE_CBC, iv.encode())
+    encrypted = aes.encrypt(msg)
+    #raise NotImplementedError("You need to implement the encrypt() function that performs AES-128 encryption")
+    return encrypted
 
-def calc_mac():
-  pass
+# string * bytes -> bytes
+def calc_mac(key, msg):
+    h = hmac.new(key.encode(), msg, hashlib.sha256) # input type of byte
+    return h.digest()
 
-def ae_encrypt():
-  pass
+# int * string * string * string * string -> bytes
+def ae_encrypt(enckey, mackey, iv, msg):
+    encrypted = None
+    msg = msg.encode()
+    mac = calc_mac(mackey, msg)
+    msg += mac 
+    encrypted = encrypt(enckey, iv, msg)
+    return encrypted
 
-def run(ae, enckey, mackey, iv, addr = "localhost", port = "22"):
-  alice = socket.socket.AF_INET, socket.SOCK_STREAM)
-  alice.connect((addt, port))
-  logging.info("[*] Client is connected to {}:{}".format(addr, port))
+def run(addr, port, enckey, mackey, iv):
+    alice = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    alice.connect((addr, port))
+    logging.info("[*] Client is connected to {}:{}".format(addr, port))
+    logging.info("[*] send GPS information")
+
+    for i in range(24):
+        #msg = "{}: {}, {}: {}, {}: {}, {}: {}".format(label_pid[i], pids[i], label_hour[i], hours[i], label_lat[i], lats[i], label_lon[i], lons[i])
+        msg = "{}: {},{}: {},{}: {},{}: {}".format(label_pid[i], pids[i], label_hour[i], hours[i], label_lat[i], lats[i], label_lon[i], lons[i])
+        logging.info("[*] Sending Data: {}".format(msg))
+        encrypted = ae_encrypt(enckey, mackey, iv, msg)
+        alice.send(encrypted)
+        received = alice.recv(7)
+        logging.info("[*] Received: {}".format(received))
+        sleep(2)
+
+def command_line_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--addr", metavar="<bob's address>", help="Bob's address", type=str, required=True)
+    parser.add_argument("-p", "--port", metavar="<bob's port>", help="Bob's port", type=int, required=True)
+    #parser.add_argument("-w", "--ae", metavar="<authenticated encryption (0: mac-then-encrypt / 1: encrypt-then-mac)>", help="Authenticated encryption (0: mac-then-encrypt / 1: encrypt-then-mac)", type=int, choices=[0, 1], required=True)
+    parser.add_argument("-x", "--enckey", metavar="<encryption key (AES-128)>", help="Encryption key (AES-128)", type=str, required=True)
+    parser.add_argument("-y", "--mackey", metavar="<mac key (HMAC-SHA256)>", help="MAC key (HMAC-SHA256)", type=str, required=True)
+    parser.add_argument("-z", "--iv", metavar="<initialization vector (16 byte)>", help="Initialization vector (16 byte)", type=str, required=True)
+    parser.add_argument("-l", "--log", metavar="<log level (DEBUG/INFO/WARNING/ERROR/CRITICAL)>", help="Log level (DEBUG/INFO/WARNING/ERROR/CRITICAL)", type=str, default="INFO")
+    args = parser.parse_args()
+    return args
+
+def main():
+    args = command_line_args()
+    log_level = args.log
+    logging.basicConfig(level=log_level)
+
+    if len(args.enckey) != ENCKEY_LENGTH:
+        logging.error("Encryption key length error (hint: AES-128): {} bytes".format(len(args.enckey)))
+        sys.exit(1)
+
+    if len(args.iv) != BLOCK_LENGTH:
+        logging.error("IV length error (hint: AES)")
+        sys.exit(1)
+
+    run(args.addr, args.port, args.enckey, args.mackey, args.iv)
+
+if __name__ == "__main__":
+    main()
+
+
+
   
-  for i in range(24):
-    msg = "{}: {}{}: {}{}: {}{}: {}".format(label_pid[i], pids[i], label_hour[i], hours[i], label_lat[i], lats[i], label_lon[i], lons[i])
-    alice.send(msg)
-    sleep(2)
-    
